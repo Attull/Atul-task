d1c548d0057aea62ef0e6df5064d3e9226b9aec5
Attull <atul852798@gmail.com>
2023-06-05 02:20:03 +0530

    adding activities in Highlight page

--- src/Components/Category/CategoryCard.js
+++ src/Components/Category/CategoryCard.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Collapse } from 'antd';
 import './category.css'
 
-export const CategoryCard = ({ name, activities }) => {
+export const CategoryCard = ({ key, name, activities }) => {
   const { Panel } = Collapse;
-
+console.log(key)
   return (
     <>
       <Collapse accordion>
-        <Panel header={name} key={name}>
+        <Panel header={name} key={key}>
           <ol>
             {
               activities.map((activity,i) => {
--- src/Components/Highlights/Activity.js
+++ src/Components/Highlights/Activity.js
@@ -2,12 +2,18 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getServerData } from '../../Helper/helper'
 import { Navbar } from '../Dashboard/Navbar'
+import { Card } from 'antd';
+import './highlight.css'
+
+const gridStyle = {
+  width: '25%',
+  textAlign: 'center',
+};
 
 export const Activity = () => {
 
   const { activity } = useParams()
-  console.log(activity)
-  const [aData, setAdata] = useState([])
+  const [aData, setAdata] = useState({})
 
   useEffect(() => {
     /** async function fetch backend data */
@@ -24,8 +30,18 @@ export const Activity = () => {
     <div className='dashboard-container'>
       <Navbar />
       <div className='heading'>
-         {activity}
+        {activity}
       </div>
+      <div className='activity-container'>
+        <Card title="Activities">
+          {
+            aData.activities?.map((data, i) => {
+              return <Card.Grid key={i} style={gridStyle}>{data.name}</Card.Grid>
+            })
+          }
+        </Card>
+      </div>
+
     </div>
   )
 }
